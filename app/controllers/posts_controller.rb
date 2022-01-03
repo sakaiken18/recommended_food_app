9277e1a1fcb3a0f0ea361b78cac4a33d1b5d1479
sakaiken <sakaiken@sakaikennoMacBook-Pro.local>
2022-01-03 09:27:43 +0900

    新規投稿機能を実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,9 +4,12 @@ class PostsController < ApplicationController
   end
 
   def new
+    @post = Post.new
   end
 
   def create
+    post = current_user.posts.create!(post_params)
+    redirect_to posts_url
   end
 
   def show
@@ -20,4 +23,12 @@ class PostsController < ApplicationController
 
   def destroy
   end
+
+
+  private
+
+  def post_params
+    params.require(:post).permit(:content)
+  end
+
 end
